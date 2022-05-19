5c6e68024ed15773bb8747ae3501d52540108c1c
faha1999 <kafaha1999@gmail.com>
2022-05-19 23:04:40 +0600

    complete hero left side

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="left">
+        <div className="name">
+          <span>Hy! I'm </span>
+          <span>Kawsar Ahmed Fahad</span>
+          <span>
+            Frontend Developer with high level of experience in web designing
+            and development, producting the Quality work
+          </span>
+        </div>
+
+        <button className="button">Hire me</button>
+      </div>
+
+      <div className="right">hi</div>
+    </div>
+  );
+};
