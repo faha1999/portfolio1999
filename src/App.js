6465806ca24aebc3309cb4db2e0488887a509bae
faha1999 <kafaha1999@gmail.com>
2022-05-21 15:32:30 +0600

    complete experience

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Hero } from './components/Hero';
 import { Navbar } from './components/Navbar';
 import { Services } from './components/Services';
+import { Experience } from './components/Experience';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Services />
+      <Experience />
     </div>
   );
 }
---
+++ src/components/Experience.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const Experience = () => {
+  return (
+    <div className="experience">
+      <div className="achievement">
+        <div className="circle">2+</div>
+        <span>years</span>
+        <span>Experience</span>
+      </div>
+
+      <div className="achievement">
+        <div className="circle">50+</div>
+        <span>completed</span>
+        <span>Projects</span>
+      </div>
+
+      <div className="achievement">
+        <div className="circle">5+</div>
+        <span>Companies</span>
+      </div>
+    </div>
+  );
+};
