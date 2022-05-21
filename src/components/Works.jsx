bc6aa5f47106ccc9aad46503860bd5279b238d1d
faha1999 <kafaha1999@gmail.com>
2022-05-21 22:36:54 +0600

    complete works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import Upwork from '../images/Upwork.png';
+import Fiverr from '../images/fiverr.png';
+import Amazon from '../images/amazon.png';
+import Shopify from '../images/Shopify.png';
+import Facebook from '../images/Facebook.png';
 
 export const Works = () => {
   return (
@@ -16,6 +21,33 @@ export const Works = () => {
         <button className="button">Hire Me</button>
 
         <div className="blur s-blur" style={{ background: '#abf1ff94' }}></div>
+      </div>
+
+      <div className="right">
+        <div className="mainCircle">
+          <div className="circle">
+            <img src={Upwork} alt="" />
+          </div>
+
+          <div className="circle">
+            <img src={Fiverr} alt="" />
+          </div>
+
+          <div className="circle">
+            <img src={Amazon} alt="" />
+          </div>
+
+          <div className="circle">
+            <img src={Shopify} alt="" />
+          </div>
+
+          <div className="circle">
+            <img src={Facebook} alt="" />
+          </div>
+        </div>
+
+        <div className="w-backCircle blueCircle"></div>
+        <div className="w-backCircle yellowCircle"></div>
       </div>
     </div>
   );
